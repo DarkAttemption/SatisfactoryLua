f05ceef4d7bb8683ffb5e34ff254da272f53f1ab
Leander Graf <grafleander1997@gmail.com>
2022-04-01 00:57:04 +0200

    Initial setup of the library, including sleep, getObj and log

---
+++ lib.lua
@@ -0,0 +1,19 @@
+function sleep(s)
+    event.pull(s)
+end
+   
+function getObj(s)
+    local res = component.proxy(component.findComponent(s))
+
+    for i,k in pairs(res) do
+        if k ~= nil then print("Object " .. s .. " found: ", k) end
+        return k
+    end
+
+    print("No object found for ", s)
+end
+
+function log(message)
+    print(message)
+    NetworkCard:send("654D04274F62C9562665E299A8024E60", 80, message) 
+end
